efbc3b89b54e3157025a2e0f70c581462b0eba37
lyg0527 <lyg0527@navre.com>
2025-06-12 12:34:21 +0900

    0.0.2

--- src/main/java/com/welab/k8s_backend_alim/api/backend/BackendAlimController.java
+++ src/main/java/com/welab/k8s_backend_alim/api/backend/BackendAlimController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @Slf4j
 @RestController
-@RequestMapping(value = "/backend/alim/v1", produces = MediaType.APPLICATION_JSON_VALUE)public class BackendAlimController {
+@RequestMapping(value = "/backend/alim/v1", produces = MediaType.APPLICATION_JSON_VALUE)
+public class BackendAlimController {
+
     @GetMapping(value = "/hello")
     public ApiResponseDto<String> hello() {
         return ApiResponseDto.createOk("알림 백엔드 서비스가 호출되었습니다");
